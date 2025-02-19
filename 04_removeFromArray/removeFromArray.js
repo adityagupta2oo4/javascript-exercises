cfa37ef3c90a3a08e3b007a9147b428c57a43386
Aditya Gupta <ag75100@gmail.com>
2025-02-19 19:36:38 +0000

    Removed element from array using .foreach() and .include()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(input_array,...args) {
+    let newArray = [];
+
+    input_array.array.forEach(element => {
+
+        if(!args.includes(element))
+            newArray.push(element);
+        
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
