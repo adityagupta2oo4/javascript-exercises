414b9888072c40d8f89814a89e3ce453cd2d8412
Aditya Gupta <ag75100@gmail.com>
2025-02-19 14:43:16 +0000

    Attempt 1 one to fix the problem no 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(input_string,repeatition) {
 
+    let output_string  = "";
+    
+    for(let i = 0; i<repeatition ;i++){
+        fin +=input_string;
+    }
+
+    
+    return output_string;
 };
 
 // Do not edit below this line
