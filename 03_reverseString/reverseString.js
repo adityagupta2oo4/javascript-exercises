00e94f5d8aa3c14309870ea593b3f2b117e656aa
Aditya Gupta <ag75100@gmail.com>
2025-02-19 19:15:00 +0000

    Attempt 1 of solving question 3 using charAt approach

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(input_string) {
+
+    let reversed_string ="";
+    let len = (input_string.length) -1;
+
+    while((len) >= 0){
+        reversed_string += input_string.charAt(len);
+        len--;
+    }
+
+    return reversed_string;
+
 
 };
 
