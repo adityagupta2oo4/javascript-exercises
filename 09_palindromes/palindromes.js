99a4e4f47a174b4d8a404296ce5c5c32896075a7
Aditya Gupta <ag75100@gmail.com>
2025-02-23 18:17:07 +0000

    Done with Question 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,47 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+    let end = str.length - 1;
+
+    let start = 0;
+
+    str = str.toLowerCase();
+
+
+
+    let flag = true;
+
+    function isAlpha(str){
+        return /^[A-Za-z0-9]+$/.test(str);
+    }
+
+    while(start<end){
+
+        //igonre if not alpha or number
+        if(isAlpha(str.charAt(start)) == false){
+            ++start;
+            continue; // to check again avoid the change of start and end
+
+        }
+        else if(isAlpha(str.charAt(end))==  false){
+            --end;
+            continue;
+
+        }
+
+        if(str.charAt(start) != str.charAt(end)){
+            flag = false;
+        }
+
+        start++;
+        end--;
+    }
+
+
+
+
+
+    return flag;
+
 
 };
 
