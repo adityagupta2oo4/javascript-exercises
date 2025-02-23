3a25fac30d7dd95ac55768f68e3b16006c9d7bc4
Aditya Gupta <ag75100@gmail.com>
2025-02-23 12:15:18 +0000

    Done With 6th question

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const div_4 = (year%4 == 0);
+    const div_100 = (year%100 == 0);
+    const div_400 = (year%400 ==0);
+
+    if(div_4){
+
+        if(div_100){
+            if(div_400){
+                return true;
+
+            }
+            return false;
+        }
+        return true;
+
+    }
+    
+    return false;
 
 };
 
